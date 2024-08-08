c13d50e79a3afff21d580d9a7658422174db60ff
TEJASWADPILLEWAR7 <tejaswadpillewar7@gmail.com>
2024-08-08 18:05:18 +0530

    Environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+
 
   return (
     <>
-    <h1>A blog webApp</h1>
+      <h1>A blog webApp</h1>
     </>
   )
 }
