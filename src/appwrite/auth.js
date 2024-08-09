d4d79c9c1fb5d9479cde720787ea570e43123378
TEJASWADPILLEWAR7 <tejaswadpillewar7@gmail.com>
2024-08-09 12:46:50 +0530

    created components and store

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from "../conf/conf";
+import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
@@ -7,7 +7,7 @@ export class AuthService {
 
   constructor() {
     this.client
-      .setEndpoint(conf.appwriteBucketId)
+      .setEndpoint(conf.appwriteUrl)
       .setProject(conf.appwriteProjectId);
 
     this.account = new Account(this.client);
